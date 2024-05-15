fde7c514aeacbc2a548d4887896595008c5612a7
joel rosa brito <jrbdeveloper@hotmail.com>
2024-05-15 15:54:09 -0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Pawns.Hubs;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+var app = builder.Build();
+
+app.MapGet("/", () => Results.Redirect("/index.htm"));
+
+app.UseStaticFiles();
+app.UseDefaultFiles();
+app.MapHub<Pawns.Hubs.PawnHub>("/pawnHub");
+app.Run();
